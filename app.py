3e4b1225677941a02f56e2203f856d1fb0742ecd
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-08-30 21:25:19 +0530

    fastapi-commit

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-import os
+# import os
 
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+# from flask import (Flask, redirect, render_template, request,
+#                    send_from_directory, url_for)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return ('hello, I am running')
+# @app.route('/')
+# def index():
+#    print('Request for index page received')
+#    return ('hello, I am running')
 
+
+
+# if __name__ == '__main__':
+#    app.run()
+
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.route("/root")
+def root():
+   print("Inside function")
+   return("Hello world")
 
 
 if __name__ == '__main__':
-   app.run()
+   uvicorn.run(app,host="0.0.0.0",port=8000)
