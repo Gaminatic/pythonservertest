67fc1e0521cac2b7ad83d0ff22cdd98aaac9de44
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-09-02 19:21:34 +0530

    db-changes

--- connection.py
+++ connection.py
@@ -73,48 +73,47 @@ async def create_pool_connection():
 
 
 
-from sqlmodel import create_engine
+# from sqlmodel import create_engine
 
-logger = logging.getLogger("app")
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger("app")
+# logger.setLevel(logging.INFO)
 
-sql_url = ""
-if os.getenv("WEBSITE_HOSTNAME"):
-    logger.info("Connecting to Azure PostgreSQL Flexible server based on AZURE_POSTGRESQL_CONNECTIONSTRING...")
-    env_connection_string = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
-    if env_connection_string is None:
-        logger.info("Missing environment variable AZURE_POSTGRESQL_CONNECTIONSTRING")
-    else:
-        # Parse the connection string
-        details = dict(item.split('=') for item in env_connection_string.split())
+# sql_url = ""
+# if os.getenv("WEBSITE_HOSTNAME"):
+#     logger.info("Connecting to Azure PostgreSQL Flexible server based on AZURE_POSTGRESQL_CONNECTIONSTRING...")
+#     env_connection_string = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
+#     if env_connection_string is None:
+#         logger.info("Missing environment variable AZURE_POSTGRESQL_CONNECTIONSTRING")
+#     else:
+#         # Parse the connection string
+#         details = dict(item.split('=') for item in env_connection_string.split())
 
-        # Properly format the URL for SQLAlchemy
-        sql_url = (
-            f"postgresql://{quote_plus(details['user'])}:{quote_plus(details['password'])}"
-            f"@{details['host']}:{details['port']}/{details['dbname']}?sslmode={details['sslmode']}"
-        )
+#         # Properly format the URL for SQLAlchemy
+#         sql_url = (
+#             f"postgresql://{quote_plus(details['user'])}:{quote_plus(details['password'])}"
+#             f"@{details['host']}:{details['port']}/{details['dbname']}?sslmode={details['sslmode']}"
+#         )
 
-else:
-    logger.info("Connecting to local PostgreSQL server based on .env file...")
-    load_dotenv()
-    POSTGRES_USERNAME = os.environ.get("PGUSER")
-    POSTGRES_PASSWORD = os.environ.get("PGPASSWORD")
-    POSTGRES_HOST = os.environ.get("PGHOST")
-    POSTGRES_DATABASE = os.environ.get("PGDATABASE")
-    POSTGRES_PORT = os.environ.get("PGPORT", 5432)
+# else:
+#     logger.info("Connecting to local PostgreSQL server based on .env file...")
+#     load_dotenv()
+#     POSTGRES_USERNAME = os.environ.get("PGUSER")
+#     POSTGRES_PASSWORD = os.environ.get("PGPASSWORD")
+#     POSTGRES_HOST = os.environ.get("PGHOST")
+#     POSTGRES_DATABASE = os.environ.get("PGDATABASE")
+#     POSTGRES_PORT = os.environ.get("PGPORT", 5432)
 
-    sql_url = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
+#     sql_url = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
 
-engine = create_engine(sql_url)
+# engine = create_engine(sql_url)
 
 
-
-async def getUsersDetails():
+async def getUsersDetails(pool:asyncpg.pool):
+    async with pool.acquire() as conn:
         try:
-            users = exec("SELECT * FROM users").all()
+            users = await conn.fetchrow("SELECT * FROM users")
             return users
 
         except asyncpg.PostgresError as e:
             raise e   
-
 
