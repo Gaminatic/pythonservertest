380626fb05eaeafcb3ac3a0fd84e81e3bc6fdfc6
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-09-02 16:35:41 +0530

    dbinsert

---
+++ connection.py
@@ -0,0 +1,65 @@
+import asyncio
+import logging
+import os
+from urllib.parse import quote_plus
+import asyncpg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_params = {
+    "database": os.getenv('PGDATABASE'),
+    "user": os.getenv('PGUSER'),
+    "password": os.getenv('PGPASSWORD'),
+    "host": os.getenv('PGHOST'),
+    "port": os.getenv('PGPORT'),
+    # "sslmode": os.getenv('SSL')  
+}
+
+# ssl_mode = os.getenv('SSL') == 'true'
+# ssl_context = ssl.create_default_context() if ssl_mode else None
+db_url = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+print("db_url",db_url)
+pool = None
+
+async def create_pool():
+    global pool
+    if pool is None:
+        pool = await asyncpg.create_pool(db_url,timeout=30)
+    return pool
+
+async def acquire_connection():
+    if pool is None:
+        await create_pool()
+    return await pool.acquire()
+
+async def release_connection(conn):
+    await pool.release(conn)
+
+async def close_pool():
+    global pool
+    if pool is not None:
+        try:
+            await asyncio.wait_for(pool.close(), timeout=10.0)
+        except asyncio.TimeoutError:
+            print("Pool close operation timed out.")
+        pool = None
+
+
+async def with_connection(func, *args, **kwargs):
+    conn = await acquire_connection()
+    try:
+        # print(f"Connection type: {type(conn)}")
+        return await func(*args, conn=conn, **kwargs)  
+    finally:
+        await release_connection(conn)
+
+
+async def getUsersDetails(conn):
+    try:
+        result = await conn.fetchrow('SELECT * FROM users')
+        return result   
+    
+    except Exception as e:
+        raise e
+
--- main.py
+++ main.py
@@ -16,16 +16,47 @@
 # if __name__ == '__main__':
 #    app.run()
 
+from contextlib import asynccontextmanager
+import os
+import asyncpg
+from dotenv import load_dotenv
 from fastapi import FastAPI
 import uvicorn
+from connection import db_url, with_connection,getUsersDetails
+
+from connection import acquire_connection, close_pool
 
 app = FastAPI()
+load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("database",os.getenv("PGDATABASE"))
+    print("db url",db_url)
+    await acquire_connection()
+    print(asyncpg.__version__)
+    try:
+        yield
+    finally:
+        await close_pool()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def root():
    print("Inside function")
    return("Hello world")
+
+
+@app.get("/getUsers")
+async def get_users():
+   print("Inside function get_users")
+   users = await with_connection(getUsersDetails)
+   return users
+
 
 
 if __name__ == '__main__':
