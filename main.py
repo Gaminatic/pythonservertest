7dc54d55e72ecd655b199b71599396b40fa7e69c
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-09-03 21:37:32 +0530

    fastapi-singleton

--- main.py
+++ main.py
@@ -4,34 +4,131 @@ import os
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://azureuser:sevenlake%40123@fitness-db-public.postgres.database.azure.com:5432/postgres?sslmode=require"
+# DATABASE_URL = "postgresql://azureuser:sevenlake%40123@fitness-db-public.postgres.database.azure.com:5432/postgres?sslmode=require"
 
-@app.on_event("startup")
-async def startup():
-    app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
+# @app.on_event("startup")
+# async def startup():
+#     app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
 
-@app.on_event("shutdown")
-async def shutdown():
-    await app.state.db_pool.close()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await app.state.db_pool.close()
 
 
-@app.get("/")
-async def root():
-    print("inside root function")
-    return("Root function")
+# @app.get("/")
+# async def root():
+#     print("inside root function")
+#     return("Root function")
     
 
-@app.get("/users")
-async def get_Users():
-    print("before get connection")
-    async with app.state.db_pool.acquire() as connection:
-        print("get user funct",DATABASE_URL)
-        query = "SELECT * FROM users"
-        result = await connection.fetchrow(query)
-        if result:
-            return dict(result)
+# @app.get("/users")
+# async def get_Users():
+#     print("before get connection")
+#     async with app.state.db_pool.acquire() as connection:
+#         print("get user funct",DATABASE_URL)
+#         query = "SELECT * FROM users"
+#         result = await connection.fetchrow(query)
+#         if result:
+#             return dict(result)
+#         else:
+#             raise HTTPException(status_code=404, detail="user not found")
+
+
+
+# import os
+
+# from flask import (Flask, redirect, render_template, request,
+#                    send_from_directory, url_for)
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def index():
+#    print('Request for index page received')
+#    return ('hello, I am running')
+
+
+
+# if __name__ == '__main__':
+#    app.run()
+
+from contextlib import asynccontextmanager
+import os
+import asyncpg
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import uvicorn
+# from connection import db_url,getUsersDetails
+# from connection import create_pool_connection
+# from connection import DatabaseConnection, getUsersDetails
+
+app = FastAPI()
+load_dotenv()
+
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     print("database",os.getenv("PGDATABASE"))
+#     print("db url",db_url)
+#     await acquire_connection()
+#     print(asyncpg.__version__)
+#     try:
+#         yield
+#     finally:
+#         await close_pool()
+
+
+# app = FastAPI(lifespan=lifespan)
+
+class DatabaseConnection:
+    _instance = None
+    _pool = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseConnection, cls).__new__(cls)
+        return cls._instance
+
+    async def get_pool(self):
+        if self._pool is None:
+            print("Creating new connection pool")
+            db_uri = os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING')
+            print("url",db_uri)
+            self._pool = await asyncpg.create_pool(dsn=db_uri,timeout = 60)
         else:
-            raise HTTPException(status_code=404, detail="user not found")
+            print("Reusing existing connection pool")
+        return self._pool
+
+    async def close_pool(self):
+        if self._pool is not None:
+            await self._pool.close()
+            self._pool = None
+            print("Connection pool closed")
+
+async def getUsersDetails(connection):
+    try:
+        users = await connection.fetchrow("SELECT * FROM users")
+        return users
+    except asyncpg.PostgresError as e:
+        raise e
+
+db_connection = DatabaseConnection()
+
+@app.get("/")
+def root():
+   print("Inside function")
+   print("db_uri",os.getenv('AZURE_POSTGRESQL_CONNECTIONSTRING'))
+   return("Hello world")
+
+
+@app.get("/getUsers")
+async def get_users():
+    pool = await db_connection.get_pool()  
+    async with pool.acquire() as connection:
+        users = await getUsersDetails(connection)
+    return users
+
 
 
 # from typing import List
