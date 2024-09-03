50aadf78f5eaff507cf82fb1a8f36beb3dcf960b
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-09-03 15:51:40 +0530

    addMiddleware

--- main.py
+++ main.py
@@ -1,14 +1,72 @@
-from fastapi import FastAPI, HTTPException
+# from fastapi import FastAPI, HTTPException
+# import asyncpg
+# import os
+
+# app = FastAPI()
+
+# DATABASE_URL = "postgresql://azureuser:sevenlake%40123@fitness-db-public.postgres.database.azure.com:5432/postgres?sslmode=require"
+
+# @app.on_event("startup")
+# async def startup():
+#     app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await app.state.db_pool.close()
+
+
+# @app.get("/")
+# async def root():
+#     print("inside root function")
+#     return("Hello world")
+    
+
+# @app.get("/users")
+# async def get_Users():
+#     print("before get connection")
+#     async with app.state.db_pool.acquire() as connection:
+#         print("get user funct",DATABASE_URL)
+#         query = "SELECT * FROM users"
+#         result = await connection.fetchrow(query)
+#         if result:
+#             return dict(result)
+#         else:
+#             raise HTTPException(status_code=404, detail="user not found")
+
+from typing import List
 import asyncpg
+from fastapi import FastAPI, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from pydantic import BaseModel
 import os
+import urllib
 
-app = FastAPI()
+# Environment variables for database connection
+# host_server = os.environ.get('host_server')
+# db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port')))
+# database_name = os.environ.get('database_name', 'fastapi')
+# db_username = urllib.parse.quote_plus(str(os.environ.get('db_username')))
+# db_password = urllib.parse.quote_plus(str(os.environ.get('db_password')))
 
-DATABASE_URL = "postgresql://azureuser:sevenlake%40123@fitness-db-public.postgres.database.azure.com:5432/postgres?sslmode=require"
+
+DATABASE_URL = f'postgresql://azureuser:sevenlake%40123@fitness-db-public.postgres.database.azure.com:5432/postgres?sslmode=require'
+
+# Initialize FastAPI app
+app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.add_middleware(GZipMiddleware)
 
 @app.on_event("startup")
 async def startup():
     app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
+
 
 @app.on_event("shutdown")
 async def shutdown():
@@ -19,18 +77,20 @@ async def shutdown():
 async def root():
     print("inside root function")
     return("Hello world")
-    
 
 @app.get("/users")
-async def get_Users():
+async def get_user_function():
+    query = "SELECT * FROM users"
+    print("query",query)
+    print("url",DATABASE_URL)
     async with app.state.db_pool.acquire() as connection:
-        print("get user funct",DATABASE_URL)
-        query = "SELECT * FROM users"
-        result = await connection.fetchrow(query)
-        if result:
-            return dict(result)
-        else:
-            raise HTTPException(status_code=404, detail="user not found")
+        user = await connection.fetchrow(query)
+    return {"users data",user}
+
+
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
