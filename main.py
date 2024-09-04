c4f000db842ef322bb9460d9ef84bf7171bd13fa
SrinidhiGanesan <srinidhiganesan2@gmail.com>
2024-09-04 16:52:57 +0530

    check-db

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 import asyncpg
 import os
 from dotenv import load_dotenv
 import psycopg2
+
+from connection import acquire_connection, close_pool, getUsersDetails, with_connection
 load_dotenv()
 
 app = FastAPI()
 
 DATABASE_URL = os.getenv("AZURE_POSTGRESQL_CONNECTIONSTRING")
-@app.on_event("startup")
-async def startup():
-    app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
+# @app.on_event("startup")
+# async def startup():
+#     app.state.db_pool = await asyncpg.create_pool(DATABASE_URL)
 
-@app.on_event("shutdown")
-async def shutdown():
-    await app.state.db_pool.close()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await app.state.db_pool.close()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("database",os.getenv("PGDATABASE"))
+    print("db url",DATABASE_URL)
+    await acquire_connection()
+    print(asyncpg.__version__)
+    try:
+        yield
+    finally:
+        await close_pool()
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 
 @app.get("/")
@@ -22,18 +40,27 @@ async def root():
     print("inside root function")
     return("Root function")
     
-
 @app.get("/users")
-async def get_Users():
-    print("before get connection")
-    async with app.state.db_pool.acquire() as connection:
-        print("get user funct",DATABASE_URL)
-        query = "SELECT * FROM users"
-        result = await connection.fetchrow(query)
-        if result:
-            return dict(result)
-        else:
-            raise HTTPException(status_code=404, detail="user not found")
+async def get_user():
+    print("inside get user function")
+    users = await with_connection(getUsersDetails)
+    return users
+
+
+
+# @app.get("/users")
+# async def get_Users():
+#     print("before get connection")
+#     async with app.state.db_pool.acquire() as connection:
+#         print("get user funct",DATABASE_URL)
+#         query = "SELECT * FROM users"
+#         result = await connection.fetchrow(query)
+#         if result:
+#             return dict(result)
+#         else:
+#             raise HTTPException(status_code=404, detail="user not found")
+
+
 
 
 
